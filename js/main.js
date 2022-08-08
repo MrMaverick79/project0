53eb515f387847f2a2903007d137908a69030ed7
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-08-08 17:36:17 +1000

    End of first day commit

--- js/main.js
+++ js/main.js
@@ -13,19 +13,23 @@ const game = {
         // row1: [null, null, null],
         // row2: [null, null, null],
         // row3: [null, null, null],
-        '0': [null, "x", "o"],
-        '1': ['x', "x", "o"],
-        '2': ['o', 'o' , "x"],
+        '0': [null, null, null],
+        '1': [null, null, null],
+        '2': [null, null , null],
     },
 
     displayBoard : function() { //for testing
         console.log(this.gameBoard);
     }, 
+
+    getValue: function (row, index) {
+        return this.gameBoard[row][index];
+    },
    
     placeMove : function(player, row, position) { //returns true or false if move was successful
         if(!game.gameBoard[row][position]){ //should check if there is something in the position
             this.gameBoard[row][position] = player; //places move
-            return true; //lets us know whether we can go ahead and place a 
+            return true; //lets us know whether we can go ahead and make the move
         }
         else {
             return false;
@@ -34,18 +38,26 @@ const game = {
     }, //end placeMove
 
     checkVictory: function() { //checks all possible victory conditions 
-       
+        let nullCount= 0;
         let s = new Set() //This set is reused to see if the contents of the different interations are the same. A set with a length === 1 means the palyer has won.
          //condition 1: if an entire row is the same
         for (keys in this.gameBoard){
             console.log(keys);
             for (let i = 0; i< Object.keys(this.gameBoard).length; i++) { //this iterates over a row
                 const element = this.gameBoard[keys][i]; //each element is an entry in one of the rows
-                if (typeof element === "string") {//to avoid null being determined as a win
-                    s.add(element);
+                
+                if (element === null) {//to avoid null being determined as a win
+                    s.add(nullCount);
+                    nullCount++;
                 }//end if 
+                else {
+                    s.add(element);
+                }
             } //end for
             if(s.size === 1){ //determine whether there is  row victory
+                console.log(s);
+                
+                console.log('Row Win!');
                 return true
             }//end if
             s.clear()
@@ -55,10 +67,16 @@ const game = {
         s.clear();
         for (let i = 0; i < Object.keys(this.gameBoard).length; i++) { 
             for (key in this.gameBoard){
+                if (this.gameBoard[key][i] === null) {//to avoid null being determined as a win
+                    s.add(nullCount);
+                    nullCount++;
+                }
+                    else{
                s.add(this.gameBoard[key][i]);
-              
+                }
             } 
             if (s.size===1) {
+                console.log('Column Win!');
                 return true;
             }
         }// end for
@@ -69,6 +87,7 @@ const game = {
             s.add(this.gameBoard[i.toString()][i])
         } 
         if (s.size ===1) {
+            console.log('Diagonal Win!');
             return true
         }
 
@@ -79,8 +98,10 @@ const game = {
             console.log('s is', s);
         } 
         if (s.size ===1) {
+            console.log('Win!');
             return true
-        }       
+        }      
+         
 
     }, //end checkVictory
 
@@ -101,7 +122,7 @@ const game = {
     },// end checkDraw
 
     togglePlayerTurn : function (){
-        this.isPlayerXTurn ? 
+       return  this.isPlayerXTurn ? this.isPlayerXTurn = false : this.isPlayerXTurn = true;
 
     }
 
@@ -110,13 +131,42 @@ const game = {
 
 const playGame = () => { //plays the game
     game.gameActive = true;
-    return;
+    
+    $('.gridItem').on('click', function(e){
+        let coords  = getCoords(e);
+
+        if(game.isPlayerXTurn){
+            if (game.placeMove('X', coords[0], parseInt(coords[1]))) {
+                $(this).html('X')
+                $(this).animate({
+                    'fontSize': '4em'
+                }, 1000)
+                game.togglePlayerTurn();
+            }
+        }
+        else if(game.placeMove('O', coords[0], parseInt(coords[1]))){
+            $(this).html('O')
+            $(this).animate({
+                'fontSize': '4em'
+            }, 1000)
+            game.togglePlayerTurn();
+        }
+
+        game.checkVictory()
+        game.checkDraw()
+    })
 }; //end playGame
 
-
-
+playGame()
 //==============UI===========//
 
 const renderBoard = () => {
-
+    //might keeep this for larger boards.
 }
+
+
+const getCoords = (e) => { //gets the x y from the clicked option
+   const coordArray= (e.target.className.split(" "));
+   return [coordArray[1], coordArray[2]];
+}
+
